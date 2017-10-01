8413597fcc2c8e1b040b1315b0a166ec328b5eb8
xuliangliang <wanxkl@gmail.com>
2017-10-01 10:26:40 +0800

    1、添加TaskConfig缓存清除 2、修改redis配置参数->测试环境 3、去除GcsTaskLog显示页面的多余字段

--- web/src/main/java/com/learning/gcs/web/Service/impl/GcsTaskConfigServiceImpl.java
+++ web/src/main/java/com/learning/gcs/web/Service/impl/GcsTaskConfigServiceImpl.java
@@ -1,18 +1,20 @@
 package com.learning.gcs.web.Service.impl;
 
-import com.learning.gcs.common.entity.GcsTask;
 import com.learning.gcs.common.entity.GcsTaskConfig;
+import com.learning.gcs.common.redis.KeyUtil;
+import com.learning.gcs.common.redis.RedisWriter;
 import com.learning.gcs.common.repository.GcsTaskConfigRepository;
 import com.learning.gcs.web.Service.GcsTaskConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Iterator;
-import java.util.List;
 @Service
 public class GcsTaskConfigServiceImpl implements GcsTaskConfigService {
     @Autowired
     GcsTaskConfigRepository gcsTaskConfigRepository;
+
+    @Autowired
+    private RedisWriter redisWriter;
+
     @Override
     public GcsTaskConfig findNewGcsTaskConfig() {
         GcsTaskConfig gcsTaskConfig = gcsTaskConfigRepository.findTopByOrderByCreateTimeDesc();
@@ -21,6 +23,7 @@ public class GcsTaskConfigServiceImpl implements GcsTaskConfigService {
 
     @Override
     public void updateById(int id,String luaUrl,int runTimeMax,int version) {
+        redisWriter.delete(KeyUtil.KEY_TASK_CONFIG);
         gcsTaskConfigRepository.updateById(id,luaUrl,runTimeMax,version);
     }
 }
